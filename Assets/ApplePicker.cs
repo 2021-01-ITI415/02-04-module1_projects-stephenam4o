93b8393f29881d2b4aa76da3d1aace12c7cd58cc
stephenam4o <60270726+stephenam4o@users.noreply.github.com>
2021-02-05 18:50:48 -0500

    Almost complete
    Mostly finished, losing a life doesn't work properly or increasing the score.

---
+++ Assets/ApplePicker.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApplePicker : MonoBehaviour
+{
+
+    public GameObject basketPrefab;
+
+    //Number of baskets or lives left, start with 3 always
+    public int lives = 3;
+    //Lower basket limit
+    public float bottomY = -15f;
+    //Space between baskets
+    public float spaceY = 2f;
+
+    public List<GameObject> basketList;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Creates the baskets with the proper spacing
+        basketList = new List<GameObject>();    //List is the combined basket objects or your remaining lives
+            for(int i = 0;  i < 3; i++)
+            {
+                GameObject basketO = Instantiate(basketPrefab) as GameObject;
+                Vector3 pos = Vector3.zero;
+                pos.y = bottomY + (spaceY * i);
+                basketO.transform.position = pos;
+                basketList.Add(basketO);
+            }
+    }
+
+    public void LoseLife()
+    {
+
+        GameObject[] apples = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject a in apples)
+        {
+            Destroy(a);
+        }
+        //Lose a life
+        int index = basketList.Count - 1;
+
+        //Reference basket object
+        GameObject basketO = basketList[index];
+
+        //Delete the lost life from the game
+        basketList.RemoveAt(index);
+        Destroy(basketO);
+
+        //If no more lives, replay
+        if(basketList.Count == 0)
+        {
+            SceneManager.LoadScene("Scenes_");
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Basket.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour
+{
+
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject scoreObject = GameObject.Find("ScoreCounter");
+
+        scoreText = scoreObject.GetComponent<Text>();
+
+        scoreText.text = "0";   //Default score is always 0
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement controlled by mouse so we need mouse input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //Z position is based on the camera
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        //Has to be converted from 2D to 3D as Unity is 3D
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //Make the basket follow the position of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision check)
+    {
+        if (check.gameObject.tag == "Apple")
+        {
+            Destroy(check.gameObject);
+            int score = int.Parse(scoreText.text);
+            score += 100;
+            scoreText.text = scoreText.ToString(); 
+        }
+    }
+}
